54432df558f7a4ccfad8623a07d8e48934479a1c
Eric S <esimonds09@gmail.com>
2023-09-08 09:42:33 -0400

    Add select box for user to select desired target language

--- components/Translation.vue
+++ components/Translation.vue
@@ -1,13 +1,26 @@
 <template>
   <v-container>
     <v-card class="pa-4">
-      <v-textarea
-        v-model="inputText"
-        rows="5"
-        outlined
-        :label="labelText"
-      ></v-textarea>
-
+      <v-row>
+        <v-col cols="12" sm="2">
+          <v-select
+            v-model="selectedLanguage"
+            :items="languageOptions"
+            label="Translate to:"
+            outlined
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-textarea
+            v-model="inputText"
+            rows="5"
+            outlined
+            :label="labelText"
+          ></v-textarea>
+        </v-col>
+      </v-row>
       <v-btn @click="translateText" color="primary">Translate</v-btn>
 
       <v-divider class="my-4"></v-divider>
@@ -31,8 +44,15 @@ export default {
   data() {
     return {
       inputText: "",
+      selectedLanguage: null,
       translatedText: "",
       errorMessage: "",
+      languageOptions: [
+        { text: "English", value: "en" },
+        { text: "German", value: "de" },
+        { text: "Dutch", value: "nl" },
+        { text: "Russian", value: "ru" },
+      ],
     };
   },
   computed: {
@@ -42,11 +62,15 @@ export default {
   },
   methods: {
     async translateText() {
-      const targetLang = "zrt"; // Replace with your desired target language code
-
+      // const targetLang = "ru"; // Replace with your desired target language code
+      if (!this.selectedLanguage) {
+        this.errorMessage = "Please select a target language!";
+        return;
+      }
+      console.log(this.selectedLanguage);
       const response = await api.post("/translate", {
         text: this.inputText,
-        target_lang: targetLang,
+        target_lang: this.selectedLanguage,
       });
       console.log(response);
       this.translatedText = response.data;
